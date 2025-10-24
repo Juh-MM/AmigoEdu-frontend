2637b88ade9618f2f2bc3357c1cb58919e7c49d7
Júlia Martins Monteiro <juliamamonteiro02@gmail.com>
2025-10-24 16:29:40 -0300

    Add metas page

--- src/components/Profile.jsx
+++ src/components/Profile.jsx
@@ -4,7 +4,7 @@ export default function Profile() {
     return(
         <div className="flex flex-row justify-end m-5">
             <div className="flex flex-row gap-2 items-center">
-                <div className="flex flex-col gap-0 items-end">
+                <div className="flex flex-col items-end">
                     <span className="text-sm text-sky-900">Nome do usuário</span>
                     <span className="text-xs">usuario@email.com</span>
                 </div>
