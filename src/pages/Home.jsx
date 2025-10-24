b0ee88a76efcdc74a7b7036fa63da266cb87f19d
Júlia Martins Monteiro <juliamamonteiro02@gmail.com>
2025-10-24 18:11:01 -0300

    Ajustes na home

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,19 +1,18 @@
 import Overview from '../components/Overview';
 import Link from '../components/Link';
-import Dashboards from '../components/Dashboards'
 
 export default function Home() {
 
 const nome = "Usuário teste";
 
     return(
-            <div className="flex flex-col gap-10 m-5 justify-center">
-                <div className="flex flex-row text-3xl gap-1">
+            <div className="flex flex-col gap-10 m-5 mt-0 justify-center">
+                <div className="flex flex-row text-4xl gap-1">
                     <h1>Olá,</h1> <h1 className="font-semibold">{nome}! 👋</h1>
                 </div>
-                <Overview />
+                
                 <Link />
-                <Dashboards/>
+                <Overview />
             </div>
     )
 }
