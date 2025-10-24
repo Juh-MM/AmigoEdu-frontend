c9cb212a4072507ad1ee40b968b08a788660df83
thymos <thymosvictor@gmail.com>
2025-10-24 18:33:16 -0300

    ranking

--- src/components/Sidebar.jsx
+++ src/components/Sidebar.jsx
@@ -5,8 +5,7 @@ import { TbWorld } from "react-icons/tb";
 import { IoSettingsOutline } from "react-icons/io5";
 import { TbLogout2 } from "react-icons/tb";
 
-import Logo from '../assets/Logo.png'
-
+import Logo from '../assets/Logo.png';
 import { Link } from 'react-router-dom';
 
 export default function Sidebar() {
@@ -15,29 +14,28 @@ export default function Sidebar() {
             <div className="flex flex-col w-full gap-10 items-center">
                 <img src={Logo} className="w-25" alt="Logo do AmigoEdu"/>
                 <div className="flex flex-col gap-5 w-full">
-                    <Link to="/" div className="flex gap-4 p-2 align-middle bg-sky-700/50 rounded-2xl px-4 hover:bg-sky-700/70 hover:cursor-pointer">
+                    <Link to="/home" className="flex gap-4 p-2 align-middle bg-sky-700/50 rounded-2xl px-4 hover:bg-sky-700/70 hover:cursor-pointer">
                         <AiOutlineHome className="text-amber-50 text-xl"/>
                         <h1 className="text-amber-50">Visão Geral</h1>
                     </Link>
-                    <Link to="/Metas" div className="flex gap-4 p-2 align-middle bg-sky-700/50 rounded-2xl px-4 hover:bg-sky-700/70 hover:cursor-pointer">
+                    <Link to="/metas" className="flex gap-4 p-2 align-middle bg-sky-700/50 rounded-2xl px-4 hover:bg-sky-700/70 hover:cursor-pointer">
                         <FaRegCheckSquare className="text-amber-50 text-lg"/>
                         <h1 className="text-amber-50">Metas</h1>
                     </Link>
-                    <Link to="/" div className="flex gap-4 p-2 align-middle bg-sky-700/50 rounded-2xl px-4 hover:bg-sky-700/70 hover:cursor-pointer">
+                    <Link to="/ranking" className="flex gap-4 p-2 align-middle bg-sky-700/50 rounded-2xl px-4 hover:bg-sky-700/70 hover:cursor-pointer">
                         <LuCrown className="text-amber-50 text-lg"/>
                         <h1 className="text-amber-50">Ranking</h1>
                     </Link>
-                    <Link to="/" div className="flex gap-4 p-2 align-middle bg-sky-700/50 rounded-2xl px-4 hover:bg-sky-700/70 hover:cursor-pointer">
+                    <Link to="/impacto" className="flex gap-4 p-2 align-middle bg-sky-700/50 rounded-2xl px-4 hover:bg-sky-700/70 hover:cursor-pointer">
                         <TbWorld className="text-amber-50 text-lg"/>
                         <h1 className="text-amber-50">Impacto</h1>
                     </Link>
-                    <Link to="/" div className="flex gap-4 p-2 align-middle bg-sky-700/50 rounded-2xl px-4 hover:bg-sky-700/70 hover:cursor-pointer">
+                    <Link to="/configuracoes" className="flex gap-4 p-2 align-middle bg-sky-700/50 rounded-2xl px-4 hover:bg-sky-700/70 hover:cursor-pointer">
                         <IoSettingsOutline  className="text-amber-50 text-lg"/>
                         <h1 className="text-amber-50">Configurações</h1>
                     </Link>
                 </div>
             </div>
-
 
             <div className="flex gap-4 p-2 w-25 justify-center bg-sky-700/50 rounded-2xl hover:bg-sky-700/70 hover:cursor-pointer">
                 <TbLogout2 className="text-amber-50 text-xl"/>
@@ -46,5 +44,3 @@ export default function Sidebar() {
         </div>
     )
 }
-
-//div className=""
