60f5db5d7444240ca1cdbba8c24145f2ba7e2de0
Júlia Martins Monteiro <juliamamonteiro02@gmail.com>
2025-11-18 00:03:47 -0300

    modified:   src/components/FormDadosPessoais.jsx 	new file:   src/services/userService.js

---
+++ src/services/userService.js
@@ -0,0 +1,10 @@
+import api from './api';
+
+export async function alterarUsuario(id, dadosUsuario) {
+  try {
+    const response = await api.put(`/usuarios/${id}`, dadosUsuario);
+    return response.data;
+  } catch (error) {
+    throw new Error(error.response?.data?.message || "Erro ao alterar os dados do usuário");
+  }
+}
