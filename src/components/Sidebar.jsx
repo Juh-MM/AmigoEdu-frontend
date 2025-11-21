c57c3072ef89af6fe8066d54e2729d50f54c0129
Cauã Souza de Assis Candido <166147628+Cauartsz@users.noreply.github.com>
2025-11-21 16:10:21 +0000

    resp mobile downbar

--- src/components/Sidebar.jsx
+++ src/components/Sidebar.jsx
@@ -9,35 +9,98 @@ import Logo from '../assets/Logo.png';
 import { Link } from 'react-router-dom';
 
 export default function Sidebar() {
-    return(
-        <div className="flex flex-col h-220 w-70 m-5 mr-0 p-5 gap-4 bg-sky-600 rounded-3xl justify-between text-sm">
-            <div className="flex flex-col w-full gap-10 items-center">
-                <img src={Logo} className="w-25" alt="Logo do AmigoEdu"/>
-                <div className="flex flex-col gap-5 w-full">
-                    <Link to="/home" className="flex gap-4 p-2 align-middle bg-sky-700/50 rounded-2xl px-4 hover:bg-sky-700/70 hover:cursor-pointer">
-                        <AiOutlineHome className="text-amber-50 text-xl"/>
-                        <h1 className="text-amber-50">Visão Geral</h1>
-                    </Link>
-                    <Link to="/metas" className="flex gap-4 p-2 align-middle bg-sky-700/50 rounded-2xl px-4 hover:bg-sky-700/70 hover:cursor-pointer">
-                        <FaRegCheckSquare className="text-amber-50 text-lg"/>
-                        <h1 className="text-amber-50">Metas</h1>
-                    </Link>
-                    <Link to="/ranking" className="flex gap-4 p-2 align-middle bg-sky-700/50 rounded-2xl px-4 hover:bg-sky-700/70 hover:cursor-pointer">
-                        <LuCrown className="text-amber-50 text-lg"/>
-                        <h1 className="text-amber-50">Ranking</h1>
-                    </Link>
-                    <Link to="/impact" className="flex gap-4 p-2 align-middle bg-sky-700/50 rounded-2xl px-4 hover:bg-sky-700/70 hover:cursor-pointer">
-                        <TbWorld className="text-amber-50 text-lg"/>
-                        <h1 className="text-amber-50">Impacto</h1>
-                    </Link>
-                    <Link to="/configuracoes" className="flex gap-4 p-2 align-middle bg-sky-700/50 rounded-2xl px-4 hover:bg-sky-700/70 hover:cursor-pointer">
-                        <IoSettingsOutline  className="text-amber-50 text-lg"/>
-                        <h1 className="text-amber-50">Configurações</h1>
-                    </Link>
-                </div>
-            </div>
-                <BotaoSair />
-            </div>
+  return (
+    <>
+      {/* SIDEBAR DESKTOP */}
+      <div
+        className="
+          hidden          /* escondida no mobile */
+          lg:flex         /* aparece apenas no desktop */
+          flex-col 
+          h-[90vh] 
+          w-64 
+          m-5 
+          mr-0 
+          p-5 
+          gap-4 
+          bg-sky-600 
+          rounded-3xl 
+          justify-between 
+          shadow-xl
+        "
+      >
+        <div className="flex flex-col w-full gap-10 items-center">
+          <img src={Logo} className="w-28" alt="Logo AmigoEdu" />
+
+          <div className="flex flex-col gap-5 w-full">
+            <SidebarItem to="/home" icon={<AiOutlineHome />} label="Visão Geral" />
+            <SidebarItem to="/metas" icon={<FaRegCheckSquare />} label="Metas" />
+            <SidebarItem to="/ranking" icon={<LuCrown />} label="Ranking" />
+            <SidebarItem to="/impact" icon={<TbWorld />} label="Impacto" />
+            <SidebarItem to="/configuracoes" icon={<IoSettingsOutline />} label="Configurações" />
+          </div>
         </div>
-    )
+
+        <BotaoSair />
+      </div>
+
+      {/* NAVBAR MOBILE (BOTTOM BAR) */}
+      <div
+        className="
+          lg:hidden 
+          fixed 
+          bottom-0 
+          left-0 
+          w-full 
+          h-16 
+          bg-sky-600 
+          shadow-xl 
+          flex 
+          items-center 
+          justify-around 
+          rounded-t-2xl
+          z-50
+        "
+      >
+        <MobileItem to="/home" icon={<AiOutlineHome />} />
+        <MobileItem to="/metas" icon={<FaRegCheckSquare />} />
+        <MobileItem to="/ranking" icon={<LuCrown />} />
+        <MobileItem to="/impact" icon={<TbWorld />} />
+        <MobileItem to="/configuracoes" icon={<IoSettingsOutline />} />
+      </div>
+    </>
+  );
+}
+
+// COMPONENTE PARA DESKTOP
+function SidebarItem({ to, icon, label }) {
+  return (
+    <Link
+      to={to}
+      className="
+        flex 
+        gap-4 
+        p-2 
+        items-center 
+        bg-sky-700/50 
+        rounded-2xl 
+        px-4 
+        hover:bg-sky-700/70 
+        cursor-pointer 
+        text-amber-50
+      "
+    >
+      <span className="text-xl">{icon}</span>
+      <h1 className="font-medium">{label}</h1>
+    </Link>
+  );
+}
+
+// COMPONENTE PARA MOBILE
+function MobileItem({ to, icon }) {
+  return (
+    <Link to={to} className="text-amber-50 flex flex-col items-center text-2xl">
+      {icon}
+    </Link>
+  );
 }
